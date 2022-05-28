88153b316e9a857bdcb6cb0dfb5b3d8fa77f0469
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-05-29 02:32:44 +0300

    added error modal component and also rectified button default type to button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -6,7 +6,7 @@ const Button = (props) => {
 	return (
 		<button
 			className={classes.button}
-			type={props.type || type}
+			type={props.type || "button"}
 			onClick={props.onClick}
 		>
 			{props.children}
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import Card from "../UI/Card"
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button"
+import ErrorModal from "../UI/ErrorModal"
 
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState("")
@@ -32,25 +33,31 @@ const AddUser = (props) => {
 	}
 
 	return (
-		<Card className={classes.input}>
-			<form onSubmit={addUserHandler}>
-				<label htmlFor="username">Username</label>
-				<input
-					value={enteredUsername}
-					id="username"
-					type="text"
-					onChange={usernameChangeHandler}
-				/>
-				<label htmlFor="age">Age (Years)</label>
-				<input
-					value={enteredAge}
-					id="age"
-					type="number"
-					onChange={ageChangeHandler}
-				/>
-				<Button type="submit">Add User</Button>
-			</form>
-		</Card>
+		<div>
+			<ErrorModal
+				title="An error has occured!"
+				message="Something missing!"
+			/>
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="username">Username</label>
+					<input
+						value={enteredUsername}
+						id="username"
+						type="text"
+						onChange={usernameChangeHandler}
+					/>
+					<label htmlFor="age">Age (Years)</label>
+					<input
+						value={enteredAge}
+						id="age"
+						type="number"
+						onChange={ageChangeHandler}
+					/>
+					<Button type="submit">Add User</Button>
+				</form>
+			</Card>
+		</div>
 	)
 }
 

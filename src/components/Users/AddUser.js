5e30b6b92781124144b71420cc646eaf49a480ff
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-05-29 03:09:58 +0300

    added conditional rendering of ErrorModal component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,6 +7,7 @@ import ErrorModal from "../UI/ErrorModal"
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState("")
 	const [enteredAge, setEnteredAge] = useState("")
+	const [error, setError] = useState()
 
 	const addUserHandler = (event) => {
 		event.preventDefault()
@@ -14,10 +15,18 @@ const AddUser = (props) => {
 			enteredUsername.trim().length === 0 ||
 			enteredAge.trim().length === 0
 		) {
+			setError({
+				title: "Invalid input",
+				message: "Please enter valid name and age (non-empty values)"
+			})
 			return
 		}
 		// the "+enteredAge" forces a conversion from string input to a number.
 		if (+enteredAge < 1) {
+			setError({
+				title: "Invalid age",
+				message: "Please enter valid age (> 0)"
+			})
 			return
 		}
 		props.onAddUser(enteredUsername, enteredAge)
@@ -32,12 +41,19 @@ const AddUser = (props) => {
 		setEnteredAge(event.target.value)
 	}
 
+	const errorHandler = () => {
+		setError(null)
+	}
+
 	return (
 		<div>
-			<ErrorModal
-				title="An error has occured!"
-				message="Something missing!"
-			/>
+			{error && (
+				<ErrorModal
+					title={error.title}
+					message={error.message}
+					onConfirm={errorHandler}
+				/>
+			)}
 			<Card className={classes.input}>
 				<form onSubmit={addUserHandler}>
 					<label htmlFor="username">Username</label>

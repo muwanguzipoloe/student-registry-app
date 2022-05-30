4bf01edfb3e3aad0883cbc6b1fa9de113e818fc1
Paul Muwanguzi <muwanguzipoloe@gmail.com>
2022-05-30 03:14:35 +0300

    by using React portal I have changed the rendering of the ModalOverlay and the ErrorModal in the root of the DOM by the help of reactDom and the index.html file

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
 import React, { Fragment } from "react"
+import ReactDOM from "react-dom"
+
 import Card from "./Card"
-import classes from "./ErrorModal.module.css"
 import Button from "./Button"
+import classes from "./ErrorModal.module.css"
+
+const Backdrop = (props) => {
+	return <div className={classes.backdrop} onClick={props.onConfirm} />
+}
+
+const ModalOverlay = (props) => {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{props.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{props.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={props.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	)
+}
 
 const ErrorModal = (props) => {
 	return (
 		<Fragment>
-			<div className={classes.backdrop} onClick={props.onConfirm}>
-				<Card className={classes.modal}>
-					<header className={classes.header}>
-						<h2>{props.title}</h2>
-					</header>
-					<div className={classes.content}>
-						<p>{props.message}</p>
-					</div>
-					<footer className={classes.actions}>
-						<Button onClick={props.onConfirm}>Okay</Button>
-					</footer>
-				</Card>
-			</div>
+			{ReactDOM.createPortal(
+				<Backdrop onClick={props.onConfirm} />,
+				document.getElementById("backdrop-root")
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					onConfirm={props.onConfirm}
+					title={props.title}
+					message={props.message}
+				/>,
+				document.getElementById("overlay-root")
+			)}
 		</Fragment>
 	)
 }
